c65faa96f751f5a9988c84fed2c46e8e626df704
Gerard Spivey <gs@gspivey.com>
2023-03-24 16:45:31 +0000

    added auth

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { Amplify } from 'aws-amplify'
+import config from './aws-exports'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -10,6 +12,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+Amplify.configure(config);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
